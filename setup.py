8e36aeba6c77d582425946417a96f12afe5a5b82
Ivan <ivan.zderadicka@gmail.com>
2020-09-06 12:06:49 +0200

    Updated setup for buiild in virtualenv

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+import numpy
 
 setup(
   name = 'adecaptcha',
   version='0.1',
   ext_modules = cythonize("adecaptcha/pwrspec.pyx"),
+  include_dirs=[numpy.get_include()]
 )
